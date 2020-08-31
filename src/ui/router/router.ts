5632c25409db8a5fe076341cf499a2c16d4bf9fb
Marton Farago <marton.farago@zyp.one>
2020-08-31 09:02:45 +0200

    added jsdoc comments

--- src/ui/router/router.ts
+++ src/ui/router/router.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { routes } from "./routes";
 
+/**
+ * Creates the router using the routes
+ */
 export const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
--- src/ui/router/routes.ts
+++ src/ui/router/routes.ts
@@ -5,10 +5,19 @@ import { EmptyLayout, NavbarLayout } from "@/ui/layouts";
 import { RouteName } from "./types";
 import { numberDecoder } from "./query";
 
+/**
+ * Extract the numeric ID from the route params and decodes it
+ * @param route the route to use for extraction
+ */
 const idFromRoute = (route: RouteLocationNormalized) => ({
   id: numberDecoder(route.params.id as string)
 });
 
+/**
+ * The application routes.
+ *
+ * The top level routers are used to set the used layout. Actual views are always children of a top level layout route.
+ */
 export const routes: RouteRecordRaw[] = [
   {
     path: "/",
