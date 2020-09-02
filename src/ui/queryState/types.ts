2ea6f1acb282b66f4718d1a82d99e47750c428f0
Marton Farago <marton.farago@zyp.one>
2020-09-02 19:33:02 +0200

    docs update

--- src/ui/queryState/types.ts
+++ src/ui/queryState/types.ts
@@ -6,8 +6,18 @@ import {
 } from "@/ui/router/query";
 import { queryState } from "./queryState";
 
+/**
+ * Creates a writable computed string variable stored in the query string
+ * @param key query string key
+ * @param fallback default value, used for fallback as well
+ */
 export const number = (key: string, fallback = 0) =>
   queryState<number>(key, numberEncoder, numberDecoder, fallback);
 
+/**
+ * Creates a writable computed number variable stored in the query string
+ * @param key query string key
+ * @param fallback default value, used for fallback as well
+ */
 export const string = (key: string, fallback = "") =>
   queryState<string>(key, stringEncoder, stringDecoder, fallback);
